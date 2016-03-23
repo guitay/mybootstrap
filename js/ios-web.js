6d94bf00cfd5339b87f7b85567f390d8b19090a0
nongfei <nongfei@gmail.com>
2016-03-23 09:49:02 +0800

    ios 页面适配

---
+++ js/ios-web.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	var browser = {
+		versions: function() {
+			var u = navigator.userAgent; /* app = navigator.appVersion; */
+			return { //移动终端浏览器版本信息
+				trident: u.indexOf('Trident') > -1, //IE内核
+				presto: u.indexOf('Presto') > -1, //opera内核
+				webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+				gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+				mobile: !!u.match(/AppleWebKit.*Mobile.*/) || !!u.match(/AppleWebKit/), //是否为移动终端
+				ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+				android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+				iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者QQHD浏览器
+				iPad: u.indexOf('iPad') > -1, //是否iPad
+				webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+			};
+		}(),
+		language: (navigator.browserLanguage || navigator.language).toLowerCase()
+	};
+	if (browser.versions.ios == true) {
+		// alert(screen.height);
+		// var height = screen.height*2-$('header').height()-$('footer').height();
+		// alert(height);
+		// $(".container").css("height",height+'px');
+		// $(".container").height(height);
+		// alert($("header").height(128));
+		// alert($("footer").height(98));
+	}
+	if (browser.versions.android == true) {
+		alert("i am android.")
+	}
+
+});
